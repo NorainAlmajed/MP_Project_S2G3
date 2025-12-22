4e6fcca238cd86d4cdc99e015ca7cb2859ff4b39
NorainAlmajed <202301660@student.polytechnic.bh>
2025-12-23 00:41:00 +0300

    save

--- ProjectSimulator/Norain/Controllers/EditUsersViewController.swift
+++ ProjectSimulator/Norain/Controllers/EditUsersViewController.swift
@@ -8,19 +8,43 @@
 import UIKit
 
 class EditUsersViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-
+    @IBOutlet weak var btnCause: UIButton!
+    
     @IBOutlet weak var ImagePickerEditView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let cause = UIMenu(title: "NGO Cause",children: [
+            UIAction(title: "Option 1") { _ in /* Handle */ },
+            UIAction(title: "Option 2") { _ in /* Handle */ }
+        ])
+        btnCause.menu = cause
+        btnCause.showsMenuAsPrimaryAction=true
         // Do any additional setup after loading the view.
     }
 
     @IBAction func btnImageEditPicker(_ sender: Any) {
+        showPhotoAlert()
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
+    }
+    
+    func showPhotoAlert(){
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {action
+            in
+            
+            
+        }))
+
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {action in
+            
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
@@ -28,6 +52,13 @@ class EditUsersViewController: UIViewController, UIImagePickerControllerDelegate
         ImagePickerEditView.image = image
         dismiss(animated: true)
     }
+    
+    @IBAction func btnCause(_ sender: Any) {
+    }
+   
+    
+    
+    
     /*
     // MARK: - Navigation
 

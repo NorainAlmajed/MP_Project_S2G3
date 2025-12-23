cce680bc294d276fe2db685ee2cc1b106fc1b03b
DanaK5 <2023000513@student.polytechnic.bh>
2025-12-23 13:49:28 +0300

    Started designing settings UI

---
+++ ZainabAlmahdi /AccountViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AccountViewController.swift
+//  ProjectSimulator
+//
+//  Created by BP-36-201-02 on 23/12/2025.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 5   // temporary number
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "AccountCell",
+            for: indexPath
+        )
+
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

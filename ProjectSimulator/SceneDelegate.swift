6e845668f6f45197a9b095ab16b990f22d4b9bcd
Zainab Osama <zainab2osama@gmail.com>
2025-12-30 15:56:09 +0100

    added app icon and still trying to fix how my pages are presented

--- ProjectSimulator/SceneDelegate.swift
+++ ProjectSimulator/SceneDelegate.swift
@@ -11,8 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-
         // Create window
         let window = UIWindow(windowScene: windowScene)
 
@@ -21,49 +19,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Initial screen: Login
         let loginVC = authStoryboard.instantiateViewController(
-        withIdentifier: "LoginViewController"
-        
+            withIdentifier: "LoginViewController"
         )
 
-<<<<<<< HEAD
-        // Embed UINvigationController
+        // embed in UINavigationController
         let navController = UINavigationController(rootViewController: loginVC)
 
         // Set as root
         window.rootViewController = navController
         self.window = window
         window.makeKeyAndVisible()
-        
-        }
-=======
-//        let storyboard = UIStoryboard(name: "Raghad1", bundle: nil)
-//
-//        guard let rootVC = storyboard.instantiateInitialViewController() else {
-//            fatalError("❌ Raghad1 storyboard has NO Initial View Controller")
-//        }
-//
-//        window?.rootViewController = rootVC
-//        window?.makeKeyAndVisible()
-
-        
-        window?.rootViewController = loginVC
-        window?.makeKeyAndVisible()
->>>>>>> main
     }
-    
-//    Norain schedule pickup tests
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        
-//        let storyboard = UIStoryboard(name: "norain-schedule-pickup", bundle: nil)
-//        let rootVC = storyboard.instantiateViewController(withIdentifier: "SchedulePickupViewController")
-//        
-//        // MUST wrap the root in a Navigation Controller
-//        let navVC = UINavigationController(rootViewController: rootVC)
-//        
-//        let window = UIWindow(windowScene: windowScene)
-//        window.rootViewController = navVC
-//        self.window = window
-//        window.makeKeyAndVisible()
-//    }
-
+}

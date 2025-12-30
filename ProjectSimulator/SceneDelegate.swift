122045ba36c5c6ef670a1c285e45d924acd7913b
NorainAlmajed <202301660@student.polytechnic.bh>
2025-12-30 08:37:01 +0300

    scene delegate comment

--- ProjectSimulator/SceneDelegate.swift
+++ ProjectSimulator/SceneDelegate.swift
@@ -4,35 +4,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-//    func scene(
-//        _ scene: UIScene,
-//        willConnectTo session: UISceneSession,
-//        options connectionOptions: UIScene.ConnectionOptions
-//    ) {
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//
-//        window = UIWindow(windowScene: windowScene)
-//
-//        let authStoryboard = UIStoryboard(name: "norain-schedule-pickup", bundle: nil)
-//        let loginVC = authStoryboard.instantiateViewController(
-//            withIdentifier: "SchedulePickupViewController"
-//        )
-//
-//        window?.rootViewController = loginVC
-//        window?.makeKeyAndVisible()
-//    }
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let storyboard = UIStoryboard(name: "norain-schedule-pickup", bundle: nil)
-        let rootVC = storyboard.instantiateViewController(withIdentifier: "SchedulePickupViewController")
-        
-        // MUST wrap the root in a Navigation Controller
-        let navVC = UINavigationController(rootViewController: rootVC)
-        
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = navVC
-        self.window = window
-        window.makeKeyAndVisible()
+
+        window = UIWindow(windowScene: windowScene)
+
+        let authStoryboard = UIStoryboard(name: "Authentication", bundle: nil)
+        let loginVC = authStoryboard.instantiateViewController(
+            withIdentifier: "LoginViewController"
+        )
+
+        window?.rootViewController = loginVC
+        window?.makeKeyAndVisible()
     }
+    
+    
+//    Norain schedule pickup tests
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        
+//        let storyboard = UIStoryboard(name: "norain-schedule-pickup", bundle: nil)
+//        let rootVC = storyboard.instantiateViewController(withIdentifier: "SchedulePickupViewController")
+//        
+//        // MUST wrap the root in a Navigation Controller
+//        let navVC = UINavigationController(rootViewController: rootVC)
+//        
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = navVC
+//        self.window = window
+//        window.makeKeyAndVisible()
+//    }
 }

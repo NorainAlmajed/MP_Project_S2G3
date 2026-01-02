96397eb49983724c8c74c3dfcd367d41be10308a
zahraa hubail <202305220@student.polytechnic.bh>
2026-01-02 13:43:12 -0800

    updates on file structure

---
+++ ProjectSimulator/Models/Notifications.swift
@@ -0,0 +1,16 @@
+//
+//  Notifications.swift
+//  ProjectSimulator
+//
+//  Created by Zahraa Hubail on 02/01/2026.
+//
+
+import Foundation
+
+
+struct AppNotification {
+    var title: String
+    var description: String
+    var date: Date
+    var userID: String
+}

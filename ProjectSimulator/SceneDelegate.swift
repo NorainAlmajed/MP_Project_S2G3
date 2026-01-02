e131021bcb75b56b3148f61f90b8fea24a39d426
Zainab Osama <zainab2osama@gmail.com>
2026-01-02 20:36:31 +0100

    brought back the green tabbar

--- ProjectSimulator/SceneDelegate.swift
+++ ProjectSimulator/SceneDelegate.swift
@@ -1,9 +1,6 @@
 import UIKit
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
-    
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -11,24 +8,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+       
+        let tabBarAppearance = UITabBar.appearance()
+        tabBarAppearance.tintColor = UIColor(named: "tabBarColor")
+        tabBarAppearance.unselectedItemTintColor =
+        UIColor(named: "tabBarColor")?.withAlphaComponent(0.4)
         
         let backAppearance = UIBarButtonItemAppearance()
         backAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
         backAppearance.highlighted.titleTextAttributes = [.foregroundColor: UIColor.clear]
-        
         let navAppearance = UINavigationBarAppearance()
-    
         navAppearance.configureWithTransparentBackground()
-        navAppearance.backgroundEffect = nil
-
-        
         navAppearance.backButtonAppearance = backAppearance
-        
         UINavigationBar.appearance().standardAppearance = navAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
         UINavigationBar.appearance().compactAppearance = navAppearance
         
-        // Back arrow color (adapts in Dark Mode automatically)
         UINavigationBar.appearance().tintColor = .label
     }
 }
+
